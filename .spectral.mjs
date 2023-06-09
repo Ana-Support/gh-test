618f6e2a5e06c044833ace9d25b5de98addfd6b9
ahernandez@striveathleticsatx.com <ana.citlalli.hernandez@gmail.com>
2023-06-09 15:39:52 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNTIyNg/spectral.js?branch_slug=master&token=61830b67-508f-4201-b2ad-9901a0c2daf7";
+export default { extends: ruleset };
